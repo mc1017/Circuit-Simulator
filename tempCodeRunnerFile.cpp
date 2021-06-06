57c73b7e8d3546e9e6cfb3fdf09ff40dd9dc2548
WEIHAN HUANG <huangweihan99@gmail.com>
2021-06-06 21:54:31 +0100

    feature: minor fix

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,7 @@
     for(int i = 0; i < magnitude.size(); i++){
          std::cout << magnitude[i] << std::endl;
     }
+
+    for(int i = 0; i < frequencies.size(); i++){
+        std::cout << frequencies[i] << std::endl;
+    }
